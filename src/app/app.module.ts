c453a5f8b0623e82945372ecd0939c9270467ebd
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2018-01-28 11:22:23 -0200

    config firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,3 @@
-import { fakeBackendProvider } from './_helpers/fake-backend';
-import { UserService } from './_services/user.service';
-import { AuthenticationService } from './_services/authentication.service';
-import { AuthGuard } from './_guards/auth.guard';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -11,10 +7,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { BaseRequestOptions, HttpModule } from '@angular/http';
-import { MockBackend, MockConnection  } from '@angular/http/testing';
 import { FormsModule } from '@angular/forms';
 
+//Firebase 
+import { AngularFireAuthModule  } from 'angularfire2/auth'
+import { AngularFireModule } from 'angularfire2';
+import { AuthService } from './auth.service';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBDyydIH5v2k-rFctWb5T7gf6BfS-2Yztc",
+  authDomain: "vetsystem-6f3a2.firebaseapp.com",
+  databaseURL: "https://vetsystem-6f3a2.firebaseio.com",
+  projectId: "vetsystem-6f3a2",
+  storageBucket: "vetsystem-6f3a2.appspot.com",
+  messagingSenderId: "608091134145"
+}
 
 @NgModule({
   declarations: [
@@ -26,16 +33,10 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
-  providers: [
-    AuthGuard, 
-    AuthenticationService, 
-    UserService,
-    fakeBackendProvider,
-    MockBackend,
-    BaseRequestOptions
-  ],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBDyydIH5v2k-rFctWb5T7gf6BfS-2Yztc",
+    authDomain: "vetsystem-6f3a2.firebaseapp.com",
+    databaseURL: "https://vetsystem-6f3a2.firebaseio.com",
+    projectId: "vetsystem-6f3a2",
+    storageBucket: "vetsystem-6f3a2.appspot.com",
+    messagingSenderId: "608091134145"
+  }
 };
