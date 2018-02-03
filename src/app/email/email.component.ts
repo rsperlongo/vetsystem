3c1548a5ff5161ca4884747ec126ebb3a1b1f3d1
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2018-02-03 10:49:11 -0200

    update components

--- src/app/email/email.component.ts
+++ src/app/email/email.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit(formData){
+    if (formData.valid) {
+      console.log(formData.value);
+      this.authService.login(
+        formData.value.email,
+        formData.value.password
+      );
+    }
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,17 @@
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 
-import { User } from '../_models/index';
-import { UserService } from '../_services/index';
-
 @Component({
-  moduleId: module.id,
+  selector: 'app-home',
   templateUrl: 'home.component.html',
 })
 
 export class HomeComponent implements OnInit {
-  users: User[] = [];
 
-  constructor(private userService: UserService) { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
-    this.userService.getUsers()
-      .subscribe(users => {
-        this.users = users;
-      });
-  }
+        this.authService.logout();
+      }
 
 }
